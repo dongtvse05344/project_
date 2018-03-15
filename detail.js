523fdf4ccd18cb3a8e039b3f03430932f4a89bfe
dongtvse05344 <32121429+dongtvse05344@users.noreply.github.com>
2018-03-15 13:54:04 +0700

    n

---
+++ detail.js
@@ -0,0 +1,28 @@
+﻿var pos =1;
+function init(){
+		setInterval(function(){ 
+
+			pos++;
+			pos = pos >  $('.qc p').length ? 1 : pos;
+			var hide = $('.active');
+			$('.qc p').removeClass('active');
+			
+			$('.qc p:nth-child('+pos+')').addClass('active');
+
+
+		}, 5000);
+		
+}
+
+$(function(){
+	$('.more img').click(function(event) {
+		$(".more li").css("opacity","1");
+		$(".more img").css("opacity","1");
+		$(this).css("opacity","0.8");
+
+		$('#main').attr({
+			'src': $(this).attr('src')
+		});
+	});
+
+})
--- menu-top.js
+++ menu-top.js
@@ -2,7 +2,6 @@
 $(function(){
 		
 	$(window).scroll(function(event) {
-		console.log($('html,body').scrollTop());
 		if ($('html,body').scrollTop() > 200)
 	 	{
 	 		$('.menu-top').addClass('hien');
